717ecae7b1f4c8036a1c2628e69edab96a1de9e2
Yang ha min <76045212+YangHamin@users.noreply.github.com>
2021-11-20 01:34:55 +0900

    Dart 로 앱만들기 기초

---
+++ main.dart
@@ -0,0 +1,36 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'first app',
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: (MyHomePage),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('first app'),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Text("hello"),
+            Text("hello"),
+            Text("hello"),
+          ],
+        ),
+      ),
+    );
+  }
+}
